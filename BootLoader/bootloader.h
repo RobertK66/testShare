8e1ff4c5a8c2988902ef95e06288eda0c6a175af
robert <robert.kralo@ms.com>
2017-07-09 18:36:50 +0200

    initial commit

---
+++ BootLoader/bootloader.h
@@ -0,0 +1,16 @@
+/*
+ * bootloader.h
+ *
+ * Created: 09.07.2017 17:42:45
+ *  Author: Robert
+ */ 
+
+
+#ifndef BOOTLOADER_H_
+#define BOOTLOADER_H_
+
+#define F_CPU 8000000UL
+
+
+
+#endif /* BOOTLOADER_H_ */
---
+++ BootLoader/main.c
@@ -0,0 +1,87 @@
+/*
+ * BootLoader.c
+ *
+ * Created: 09.07.2017 16:46:31
+ * Author : Robert
+ */ 
+#include "bootloader.h"
+
+//#define BV(x) (1<<x)
+//#define RED_OFF (PORTC |= BV(5))
+//#define RED_ON  (PORTC &= ~(BV(5)))
+//#define YELLOW_OFF (PORTC |= BV(4))
+//#define YELLOW_ON  (PORTC &= ~(BV(4)))
+//#define GREEN_OFF (PORTC |= BV(3))
+//#define GREEN_ON  (PORTC &= ~(BV(3)))
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/boot.h>
+#include <util/delay.h>
+#include "uart.h"
+ 
+#define BOOT_UART_BAUD_RATE     19200     /* Baudrate */
+#define XON                     17       /* XON Zeichen */
+#define XOFF                    19       /* XOFF Zeichen */
+
+int main()
+{
+    unsigned int 	c=0;            /* Empfangenes Zeichen + Statuscode */
+    unsigned char	temp;           /* Variable */
+	unsigned char   flag=1;         /* Flag zum steuern der Endlosschleife */
+//	unsigned char   p_mode=0;	    /* Flag zum steuern des Programmiermodus */
+    void (*start)( void ) = 0x0000; /* Funktionspointer auf 0x0000 */
+ 
+    /* Interrupt Vektoren verbiegen */
+
+    char sregtemp = SREG;
+    cli();
+    temp = GICR ;
+    GICR  = temp | (1<<IVCE);
+    GICR  = temp | (1<<IVSEL);
+    SREG = sregtemp;
+ 
+    /* Einstellen der Baudrate und aktivieren der Interrupts */
+    uart_init( UART_BAUD_SELECT(BOOT_UART_BAUD_RATE,F_CPU) ); 
+    sei();
+ 
+    uart_puts("Hallo hier ist der Bootloader\n\r");
+    _delay_ms(1000);
+ 
+    do
+    {
+        c = uart_getc();
+        if( !(c & UART_NO_DATA) )
+        {
+            switch((unsigned char)c)
+            {
+                 case 'q': 
+					 flag=0;
+                     uart_puts("Verlasse den Bootloader!\n\r");
+                     break;
+                  default:
+                     uart_puts("Du hast folgendes Zeichen gesendet: ");
+                     uart_putc((unsigned char)c);
+                     uart_puts("\n\r");
+                     break;
+            }
+        }
+    }
+    while(flag);
+ 
+    uart_puts("Springe zur Adresse 0x0000!\n\r");
+    _delay_ms(1000);
+
+    /* vor Rücksprung eventuell benutzte Hardware deaktivieren
+       und Interrupts global deaktivieren, da kein "echter" Reset erfolgt */
+
+    /* Interrupt Vektoren wieder gerade biegen */
+    cli();
+    temp = GICR ;
+    GICR  = temp | (1<<IVCE);
+    GICR  = temp & ~(1<<IVSEL);
+
+    /* Rücksprung zur Adresse 0x0000 */
+    start(); 
+    return 0;
+}
