90b8b3bef3f4796bca37c9c9cf9b93667111bbd7
robert <robert.kralo@ms.com>
2017-07-09 19:51:33 +0200

    bootloader section mit app gemeinsam funkt. (Prog mit bootloader at c00)

--- BootLoader/main.c
+++ BootLoader/main.c
@@ -6,13 +6,13 @@
  */ 
 #include "bootloader.h"
 
-//#define BV(x) (1<<x)
-//#define RED_OFF (PORTC |= BV(5))
-//#define RED_ON  (PORTC &= ~(BV(5)))
-//#define YELLOW_OFF (PORTC |= BV(4))
-//#define YELLOW_ON  (PORTC &= ~(BV(4)))
-//#define GREEN_OFF (PORTC |= BV(3))
-//#define GREEN_ON  (PORTC &= ~(BV(3)))
+#define BV(x) (1<<x)
+#define RED_OFF (PORTC |= BV(5))
+#define RED_ON  (PORTC &= ~(BV(5)))
+#define YELLOW_OFF (PORTC |= BV(4))
+#define YELLOW_ON  (PORTC &= ~(BV(4)))
+#define GREEN_OFF (PORTC |= BV(3))
+#define GREEN_ON  (PORTC &= ~(BV(3)))
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -20,11 +20,29 @@
 #include <util/delay.h>
 #include "uart.h"
  
-#define BOOT_UART_BAUD_RATE     19200     /* Baudrate */
+#define BOOT_UART_BAUD_RATE     9600     /* Baudrate */
 #define XON                     17       /* XON Zeichen */
 #define XOFF                    19       /* XOFF Zeichen */
 
-int main()
+int mainApp() __attribute__((section(".myapp")));
+//
+int mainApp() {
+	DDRC = 0xFF;
+	PORTC = 0xFF;
+		
+	int i = 10;
+	while (i-- > 0 ) {
+		RED_ON;
+		_delay_ms(1000);
+		RED_OFF;
+		_delay_ms(400);
+	}
+	main();
+}
+
+
+
+int main() 
 {
     unsigned int 	c=0;            /* Empfangenes Zeichen + Statuscode */
     unsigned char	temp;           /* Variable */
@@ -83,5 +101,6 @@ int main()
 
     /* Rücksprung zur Adresse 0x0000 */
     start(); 
+	mainApp();
     return 0;
 }
